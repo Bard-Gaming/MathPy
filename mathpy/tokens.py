24f0ffb701e8de9c59127c48d40bd2c52b9f4d8c
Bard-Gaming <christophe@dronne.fr>
2023-10-29 18:18:05 +0100

    small change to token position (doesn't affect anything other than error messages)

--- mathpy/tokens.py
+++ mathpy/tokens.py
@@ -10,7 +10,7 @@ class Token:
         return self.value
 
     def get_position(self) -> tuple[int, int]:
-        return self.line, self.index
+        return self.line + 1, self.index  # starts at 1 and not 0
 
     def __repr__(self) -> str:
         return f'Token({self.value !r}, {self.tt_type !r})'
